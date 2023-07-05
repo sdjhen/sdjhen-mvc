0202735c4e7c0fb5a377ccd0213f9ceee8e8d3e9
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-07-05 16:36:31 +0100

    added to config & added autoload function for core libs

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost:8080/sdjhen-mvc');
+
+// Site Name
+define('SITENAME', 'sdjhen-mvc');
